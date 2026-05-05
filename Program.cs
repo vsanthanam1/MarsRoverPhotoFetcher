f5df2dac338ec8208ed0544066595d0851018f85
vsanthanam1 <36714058+vsanthanam1@users.noreply.github.com>
2026-05-05 16:17:32 -0500

    Update Program.cs to include CORS

--- Program.cs
+++ Program.cs
@@ -21,7 +21,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAngular");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
